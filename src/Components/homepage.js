37a94954fb4b4f9c531ea28994fed72aa8bf8e70
alejandro-0502 <gabumon28@gmail.com>
2023-06-15 21:47:57 -0700

    Initialization + start of sidebar

---
+++ src/Components/homepage.js
@@ -0,0 +1,12 @@
+import './homepage.css';
+import Sidebar from './sidebar.js';
+
+function Homepage() {
+  return (
+    <div className='Homepage'>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/Components/sidebar.js
@@ -0,0 +1,8 @@
+import "./sidebar.css"
+function Sidebar() {
+    return (
+        <div className="Sidebar"></div>
+    )
+}
+
+export default Sidebar;
